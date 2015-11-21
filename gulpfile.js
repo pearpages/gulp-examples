8e6c1654d94c024950bf38ba1da48e92c843a1c8
pearpages <pere@soms.cat>
2015-11-21 17:01:49 +0100

    adding more gulp dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,45 @@
-(function (){
-	var gulp = require('gulp');
+(function () {
+    var gulp = require('gulp');
+    var args = require('yargs').argv;
 
-	gulp.task('hello-world', function() {
-		console.log('My first gulp task!');
-	});
+    var $ = require('gulp-load-plugins')({lazy: true}); // load on demand
+
+    // var jshint = require('gulp-jshint'); //code correctness
+    // var jscs = require('gulp-jscs'); //code structure
+    // var util = require('gulp-util');
+    // var gulpPrint = require('gulp-print'); //show all t he files we are 'touching'
+    // var gulpif = require('gulp-if'); //for conditionals statements
+
+    gulp.task('hello-world', function() {
+        console.log('My first gulp task!');
+    });
+
+    gulp.task('vet', function() {
+        log('Analyzing source with JSHint and JSCS');
+
+        //all the source code + the root js files
+        gulp.src([
+            './src/**/*.js',
+            './*.js'
+        ])
+        .pipe(gulpif(args.verbose, gulpPrint()))
+        .pipe(jscs())
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish', {verbose:true}))
+        .pipe(jshint.reporter('fail'));
+    });
+
+    /* Helpers */
+
+    function log(msg) {
+        if (typeof(msg) === 'object') {
+            for (var item in msg) {
+                if (msg.hasOwnProperty(item)) {
+                    util.log(util.colors.blue(msg[item]));
+                }
+            }
+        } else {
+            util.log(util.colors.blue(msg));
+        }
+    }
 }());
---
+++ src/code-with-errors.js
@@ -0,0 +1,13 @@
+(function (){
+
+    var hello;
+
+    if (false) {
+        var x = 1;
+    }
+
+    hello = (function () {
+        var there;
+    }());
+
+}());
